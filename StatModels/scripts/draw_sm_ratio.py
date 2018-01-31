144c6a7ac780cc705fd5e944c59581f5061f553e
Maria Teresa Grippo <mgrippo@users.noreply.github.com>
2018-01-31 12:40:24 +0100

    Improved Limits setup (#1)
    - StatModels/scripts/cmb_signal_points.py: added loop to combine all mass points per channel and category
    - StatModels/scripts/draw_sm.py: added #!usr...
    - StatModels/scripts/draw_sm_ratio.py: implemented code to draw the sm limits and the ratio with refernce limits to see the changes (y axis and distance between pads can be still improved)
    - StatModels/scripts/run_res_ttbb.py: modified ranges and MVA SETUP and NAMES has to be changed accordingly inside the code
    - StatModels/scripts/run_res_ttbb_tot.py: created code to run "*_tot_*" mva setups

--- StatModels/scripts/draw_sm_ratio.py
+++ StatModels/scripts/draw_sm_ratio.py
@@ -4,24 +4,26 @@ import os
 import argparse
 import json
 import ROOT
+import CombineHarvester.CombineTools.plotting as plot
 
 parser = argparse.ArgumentParser(description='Draw sm limits.',
                   formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position=30, width=90))
 parser.add_argument('--input', required=True, dest='input', type=str, metavar='PATH', help="input path")
+parser.add_argument('--input_ref', required=True, dest='input_ref', type=str, metavar='PATH', help="input path ref ")
 args = parser.parse_args()
 
 output = '{}/limits_sm.pdf'.format(args.input)
 
-c1 = ROOT.TCanvas("", "", 700, 700)
+ROOT.gROOT.SetBatch(True)
+
+c1 = ROOT.TCanvas("", "", 800, 900)
 c1.SetGridx()
 c1.SetFrameLineWidth(2)
-c1.SetLeftMargin(0.15)
+c1.SetLeftMargin(0.1)
 c1.SetRightMargin(0.05)
 c1.SetTopMargin(0.05)
 c1.SetTicky(1)
-c1.SetLogy()
 ROOT.gStyle.SetEndErrorSize(0)
-
 
 c1.Print(output + '[')
 
@@ -29,29 +31,37 @@ c1.Draw()
 c1.cd()
 
 
+main_pad = ROOT.TPad("main_pad", "", 0.0, 0.35, 1, 1)
+main_pad.SetGridx()
+main_pad.Draw()
+main_pad.cd()
+main_pad.SetLogy();
+
 obs = False
 n_bins = 4
 frame = ROOT.TH1F('hframe', '', n_bins, 0, n_bins)
+frame.GetXaxis().SetNdivisions(4)
+frame.GetXaxis().SetBinLabel(1, ' ')
+frame.GetXaxis().SetBinLabel(2, ' ')
+frame.GetXaxis().SetBinLabel(3, ' ')
+frame.GetXaxis().SetBinLabel(4, ' ')
 frame.SetMinimum(7)
 frame.SetMaximum(500)
 frame.SetStats(0)
-frame.GetXaxis().SetNdivisions(4)
-frame.GetXaxis().SetBinLabel(1, 'e#tau')
-frame.GetXaxis().SetBinLabel(2, '#mu#tau')
-frame.GetXaxis().SetBinLabel(3, '#tau#tau')
-frame.GetXaxis().SetBinLabel(4, 'cmb')
-frame.GetXaxis().SetLabelSize(0.1)
-frame.GetXaxis().SetLabelOffset(0.01)
+
 
 
 frame.GetYaxis().SetTicks("+")
 frame.GetYaxis().SetLabelOffset(-0.04)
 frame.GetYaxis().SetLabelSize(0.03)
 frame.GetYaxis().SetTitle("95% CL limit on #sigma / #sigma_{SM}")
-frame.GetYaxis().SetTitleOffset(-1.2)
+frame.GetYaxis().SetTitleOffset(-0.9)
 frame.GetYaxis().SetTitleSize(0.05)
 
 frame.Draw()
+
+l = ROOT.TLine(0.5,0.5,1,1)
+l.Draw()
 
 graphs = {}
 if obs:
@@ -70,7 +80,7 @@ for ch in [ 'eTau', 'muTau', 'tauTau', 'cmb' ]:
         graphs['exp0'].SetPoint(n, n+0.5, exp)
         graphs['exp1'].SetPoint(n, n+0.5, exp)
         graphs['exp2'].SetPoint(n, n+0.5, exp)
-
+        
         if obs:
             graphs['obs'].SetPointEXlow(n, width/2)
             graphs['obs'].SetPointEXhigh(n, width/2)
@@ -95,6 +105,7 @@ if obs:
 
 graphs['exp0'].SetLineColor(ROOT.kRed)
 graphs['exp0'].SetMarkerColor(ROOT.kRed)
+graphs['exp0'].SetMarkerStyle(8)
 graphs['exp1'].SetFillColor(ROOT.kYellow)
 graphs['exp2'].SetFillColor(ROOT.kGreen)
 
@@ -146,8 +157,8 @@ legend.Draw()
 
 def DrawCMSLogo(pad, cmsText, extraText, iPosX, relPosX, relPosY, relExtraDY, extraText2='', cmsTextSize=0.8):
     """Blah
-
-    Args:
+        
+        Args:
         pad (TYPE): Description
         cmsText (TYPE): Description
         extraText (TYPE): Description
@@ -157,30 +168,30 @@ def DrawCMSLogo(pad, cmsText, extraText, iPosX, relPosX, relPosY, relExtraDY, ex
         relExtraDY (TYPE): Description
         extraText2 (str): Description
         cmsTextSize (float): Description
-
-    Returns:
+        
+        Returns:
         TYPE: Description
-    """
+        """
     pad.cd()
     cmsTextFont = 62  # default is helvetic-bold
-
+    
     writeExtraText = len(extraText) > 0
     writeExtraText2 = len(extraText2) > 0
     extraTextFont = 52
-
+    
     # text sizes and text offsets with respect to the top frame
     # in unit of the top margin size
     lumiTextOffset = 0.2
     # cmsTextSize = 0.8
     # float cmsTextOffset    = 0.1;  // only used in outOfFrame version
-
+    
     # ratio of 'CMS' and extra text size
     extraOverCmsTextSize = 0.76
-
+    
     outOfFrame = False
     if iPosX / 10 == 0:
         outOfFrame = True
-
+    
     alignY_ = 3
     alignX_ = 2
     if (iPosX / 10 == 0):
@@ -197,29 +208,29 @@ def DrawCMSLogo(pad, cmsText, extraText, iPosX, relPosX, relPosY, relExtraDY, ex
         alignX_ = 3
     # if (iPosX == 0): relPosX = 0.14
     align_ = 10 * alignX_ + alignY_
-
+    
     l = pad.GetLeftMargin()
     t = pad.GetTopMargin()
     r = pad.GetRightMargin()
     b = pad.GetBottomMargin()
-
+    
     latex = ROOT.TLatex()
     latex.SetNDC()
     latex.SetTextAngle(0)
     latex.SetTextColor(ROOT.kBlack)
-
+    
     extraTextSize = extraOverCmsTextSize * cmsTextSize
     pad_ratio = (float(pad.GetWh()) * pad.GetAbsHNDC()) / \
         (float(pad.GetWw()) * pad.GetAbsWNDC())
     if (pad_ratio < 1.):
         pad_ratio = 1.
-
+    
     if outOfFrame:
         latex.SetTextFont(cmsTextFont)
         latex.SetTextAlign(11)
         latex.SetTextSize(cmsTextSize * t * pad_ratio)
         latex.DrawLatex(l, 1 - t + lumiTextOffset * t, cmsText)
-
+    
     posX_ = 0
     if iPosX % 10 <= 1:
         posX_ = l + relPosX * (1 - l - r)
@@ -227,7 +238,7 @@ def DrawCMSLogo(pad, cmsText, extraText, iPosX, relPosX, relPosY, relExtraDY, ex
         posX_ = l + 0.5 * (1 - l - r)
     elif (iPosX % 10 == 3):
         posX_ = 1 - r - relPosX * (1 - l - r)
-
+    
     posY_ = 1 - t - relPosY * (1 - t - b)
     if not outOfFrame:
         latex.SetTextFont(cmsTextFont)
@@ -238,11 +249,9 @@ def DrawCMSLogo(pad, cmsText, extraText, iPosX, relPosX, relPosY, relExtraDY, ex
             latex.SetTextFont(extraTextFont)
             latex.SetTextAlign(align_)
             latex.SetTextSize(extraTextSize * t * pad_ratio)
-            latex.DrawLatex(
-                posX_, posY_ - relExtraDY * cmsTextSize * t, extraText)
-            if writeExtraText2:
-                latex.DrawLatex(
-                    posX_, posY_ - 1.8 * relExtraDY * cmsTextSize * t, extraText2)
+            latex.DrawLatex(posX_, posY_ - relExtraDY * cmsTextSize * t, extraText)
+        if writeExtraText2:
+            latex.DrawLatex(posX_, posY_ - 1.8 * relExtraDY * cmsTextSize * t, extraText2)
     elif writeExtraText:
         if iPosX == 0:
             posX_ = l + relPosX * (1 - l - r)
@@ -254,5 +263,74 @@ def DrawCMSLogo(pad, cmsText, extraText, iPosX, relPosX, relPosY, relExtraDY, ex
 
 DrawCMSLogo(c1, 'CMS', "Internal", 11, 0.045, 0.035, 1.2, '', 1.2)
 
+
+c1.cd()
+ratio_pad = ROOT.TPad("ratio_pad", "", 0.0, 0.05, 1, 0.35)
+ratio_pad.SetGridx()
+ratio_pad.Draw()
+ratio_pad.cd()
+
+obs = False
+n_bins = 4
+frame_1 = ROOT.TH1F('hframe_1', '', n_bins, 0, n_bins)
+frame_1.SetMinimum(0.4)
+frame_1.SetMaximum(1)
+frame_1.SetStats(0)
+frame_1.GetXaxis().SetNdivisions(4)
+frame_1.GetXaxis().SetBinLabel(1, 'e#tau')
+frame_1.GetXaxis().SetBinLabel(2, '#mu#tau')
+frame_1.GetXaxis().SetBinLabel(3, '#tau#tau')
+frame_1.GetXaxis().SetBinLabel(4, 'cmb')
+frame_1.GetXaxis().SetLabelSize(0.15)
+frame_1.GetXaxis().SetLabelOffset(0.01)
+
+
+frame_1.GetYaxis().SetTicks("+")
+frame_1.GetYaxis().SetLabelOffset(-0.03)
+frame_1.GetYaxis().SetLabelSize(0.05)
+frame_1.GetYaxis().SetTitle("Ratio")
+frame_1.GetYaxis().SetTitleOffset(-0.4)
+frame_1.GetYaxis().SetTitleSize(0.12)
+
+frame_1.Draw()
+
+graphs_1 = {}
+if obs:
+    graphs_1['obs'] = ROOT.TGraphAsymmErrors()
+graphs_1['exp0'] = ROOT.TGraphAsymmErrors()
+h = 0
+width = 0.95
+for ch in [ 'eTau', 'muTau', 'tauTau', 'cmb' ]:
+    with open('{}/limits_{}.json'.format(args.input, ch), 'r') as f:
+        res = json.load(f)['1.0']
+        exp = res['exp0']
+    with open('{}/CMS-HIG-17-002_SM_{}.json'.format(args.input_ref, ch), 'r') as f_1:
+        res_1 = json.load(f_1)['1.0']
+        exp_1 = res_1['exp0']
+        graphs_1['exp0'].SetPoint(h, h+0.5, exp/exp_1)
+        graphs_1['exp0'].SetPointEXlow(h, width/2)
+        graphs_1['exp0'].SetPointEXhigh(h, width/2)
+    
+    h += 1
+
+if obs:
+    graphs_1['obs'].SetMarkerColor(ROOT.kBlack)
+    graphs_1['obs'].SetMarkerStyle(8)
+    graphs_1['obs'].SetMarkerSize(0.8)
+
+graphs_1['exp0'].SetLineColor(ROOT.kRed)
+graphs_1['exp0'].SetMarkerStyle(8)
+graphs_1['exp0'].SetMarkerColor(ROOT.kRed)
+
+graphs_1['exp0'].Draw("p same")
+if obs:
+    graphs_1['obs'].Draw("p same")
+
+
+c1.Draw()
+
 c1.Print(output)
 c1.Print(output + ']')
+
+
+
